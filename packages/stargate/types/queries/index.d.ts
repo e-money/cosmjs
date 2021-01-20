c43ad1fb6e1f94db569a409261b7c2618ef12394
willclarktech <willclarktech@users.noreply.github.com>
2021-01-20 17:39:32 +0000

    WIP: clear types

--- packages/stargate/types/queries/index.d.ts
+++
@@ -1,4 +0,0 @@
-export { QueryClient } from "./queryclient";
-export { AuthExtension, setupAuthExtension } from "./auth";
-export { BankExtension, setupBankExtension } from "./bank";
-export { IbcExtension, setupIbcExtension } from "./ibc";
